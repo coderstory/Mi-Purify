4b3da3211e6877dc5b3c15b12267660db4a4b776
coderstory <coderstory@163.com>
2017-11-18 15:01:15 +0800

    移除2个禁用钩子情况下无法使用的功能
    (cherry picked from commit f680c8c)

--- app/src/main/java/com/coderstory/Purify/fragment/OthersFragment.java
+++ app/src/main/java/com/coderstory/Purify/fragment/OthersFragment.java
@@ -14,22 +14,8 @@ public class OthersFragment extends BaseFragment {
     @Override
     protected void setUpView() {
 
-
-        $(R.id.RemoveSearchBar).setOnClickListener(v -> {
-            getEditor().putBoolean("RemoveSearchBar", ((Switch) v).isChecked());
-            getEditor().apply();
-            sudoFixPermissions();
-        });
-
         $(R.id.fixpcb).setOnClickListener(v -> {
             getEditor().putBoolean("fixpcb", ((Switch) v).isChecked());
-            getEditor().apply();
-            sudoFixPermissions();
-        });
-
-
-        $(R.id.root25).setOnClickListener(v -> {
-            getEditor().putBoolean("root25", ((Switch) v).isChecked());
             getEditor().apply();
             sudoFixPermissions();
         });
@@ -72,9 +58,7 @@ public class OthersFragment extends BaseFragment {
 
     @Override
     protected void setUpData() {
-        ((Switch) $(R.id.RemoveSearchBar)).setChecked(getPrefs().getBoolean("RemoveSearchBar", false));
         ((Switch) $(R.id.fixpcb)).setChecked(getPrefs().getBoolean("fixpcb", false));
-        ((Switch) $(R.id.root25)).setChecked(getPrefs().getBoolean("root25", false));
         ((Switch) $(R.id.hide_icon_label)).setChecked(getPrefs().getBoolean("hide_icon_label", false));
         ((Switch) $(R.id.authcreak)).setChecked(getPrefs().getBoolean("authcreak", false));
         ((Switch) $(R.id.zipauthcreak)).setChecked(getPrefs().getBoolean("zipauthcreak", false));
