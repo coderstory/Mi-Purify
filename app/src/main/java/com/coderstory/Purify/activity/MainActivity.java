b9152c704dbae6052ab4bb8bab81c3299fd78d9a
qianyf <qianyf@ywsoftware.com>
2017-12-06 10:22:58 +0800

    合并 核心破解的更改
    (cherry picked from commit a9c4cde)

--- app/src/main/java/com/coderstory/Purify/activity/MainActivity.java
+++ app/src/main/java/com/coderstory/Purify/activity/MainActivity.java
@@ -46,25 +46,38 @@ public class MainActivity extends BaseActivity {
     @SuppressLint("HandlerLeak")
     Handler myHandler = new Handler() {
         public void handleMessage(Message msg) {
-
-            if (msg.arg1 == 0) {
-                final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
-                normalDialog.setTitle("提示");
-                normalDialog.setMessage("请先授权应用ROOT权限");
-                normalDialog.setPositiveButton("确定",
-                        (dialog, which) -> System.exit(0));
-                // 显示
-                normalDialog.show();
-                super.handleMessage(msg);
-            } else if ((msg.arg1 == 1)) {
-                dialog = ProgressDialog.show(MainActivity.this, "检测ROOT权限", "请在ROOT授权弹窗中给与ROOT权限,\n如果长时间无反应则请检查ROOT程序是否被\"省电程序\"干掉");
-                dialog.show();
-            } else {
-                if (dialog != null && dialog.isShowing()) {
-                    dialog.cancel();
-                    getEditor().putBoolean("isRooted",true).apply();
-                }
+            switch (msg.arg1) {
+                case 0:
+                    final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+                    normalDialog.setTitle("提示");
+                    normalDialog.setMessage("请先授权应用ROOT权限");
+                    normalDialog.setPositiveButton("确定",
+                            (dialog, which) -> System.exit(0));
+                    normalDialog.show();
+                    super.handleMessage(msg);
+                    break;
+                case 1:
+                    dialog = ProgressDialog.show(MainActivity.this, "检测ROOT权限", "请在ROOT授权弹窗中给与ROOT权限,\n如果长时间无反应则请检查ROOT程序是否被\"省电程序\"干掉");
+                    dialog.show();
+                    break;
+                case 2:
+                    if (dialog != null && dialog.isShowing()) {
+                        dialog.cancel();
+                        getEditor().putBoolean("isRooted", true).apply();
+                    }
+                    break;
+                case 3:
+                    android.app.AlertDialog.Builder dialog = new android.app.AlertDialog.Builder(MainActivity.this);
+                    dialog.setTitle("提示");
+                    dialog.setMessage("本应用尚未再Xposed中启用,请开始后再试...");
+                    dialog.setPositiveButton("退出", (dialog12, which) -> {
+                        System.exit(0);
+                    });
+                    dialog.setCancelable(false);
+                    dialog.show();
+                    break;
             }
+
         }
     };
     private DrawerLayout mDrawerLayout;//侧边菜单视图
@@ -113,9 +126,6 @@ public class MainActivity extends BaseActivity {
         mDrawerLayout = $(R.id.drawer_layout);
         mNavigationView = $(navigation_view);
 
-        if (getPrefs().getBoolean("enableCheck", true) && !isEnable()) {
-            SnackBarUtils.makeLong(mNavigationView, "插件尚未激活,Xposed功能将不可用,请重启再试！").show();
-        }
 
         mToolbar.setTitle(getString(R.string.othersettings));
 
@@ -136,7 +146,7 @@ public class MainActivity extends BaseActivity {
         if (navigationMenuView != null) {
             navigationMenuView.setVerticalScrollBarEnabled(false);
         }
-        if (!getPrefs().getBoolean("isRooted",false)) {
+        if (!getPrefs().getBoolean("isRooted", false)) {
             // 检测弹窗
             new Thread(() -> {
                 Message msg = new Message();
@@ -151,14 +161,14 @@ public class MainActivity extends BaseActivity {
                     msg.arg1 = 2;
                     myHandler.sendMessage(msg);
                 }
+                checkEnable();
             }).start();
+        } else {
+            checkEnable();
         }
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!(MainActivity.this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
-                requestCameraPermission();
-            }
-        }
+    private void checkEnable() {
     }
 
     //init the default checked fragment
@@ -178,7 +188,6 @@ public class MainActivity extends BaseActivity {
             if (null != mPreMenuItem) {
                 mPreMenuItem.setChecked(false);
             }
-
             if (Misc.isProcessing) {
                 SnackBarUtils.makeShort(mDrawerLayout, getString(R.string.isWorkingTips)).danger();
                 return false;
--- app/src/main/java/com/coderstory/Purify/module/CorePatch.java
+++ app/src/main/java/com/coderstory/Purify/module/CorePatch.java
@@ -7,9 +7,9 @@ import java.lang.reflect.Field;
 
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 
@@ -22,7 +22,7 @@ public class CorePatch extends XposedHelper implements IModule {
             protected void beforeHookedMethod(MethodHookParam methodHookParam)
                     throws Throwable {
                 prefs.reload();
-                if (prefs.getBoolean("authcreak", false)) {
+                if (prefs.getBoolean("authcreak", true)) {
                     methodHookParam.setResult(true);
                 }
             }
@@ -32,24 +32,64 @@ public class CorePatch extends XposedHelper implements IModule {
             protected void beforeHookedMethod(MethodHookParam paramAnonymousMethodHookParam)
                     throws Throwable {
                 prefs.reload();
-                if (prefs.getBoolean("authcreak", false)) {
+                if (prefs.getBoolean("authcreak", true)) {
                     paramAnonymousMethodHookParam.setResult(true);
+                }
+            }
+        });
+
+        final Class ApkSignatureSchemeV2Verifier = XposedHelpers.findClass("android.util.apk.ApkSignatureSchemeV2Verifier", null);
+        final Class packageParser = XposedHelpers.findClass("android.content.pm.PackageParser", null);
+        final Class strictJarVerifier = XposedHelpers.findClass("android.util.jar.StrictJarVerifier", null);
+        final Class packageClass = XposedHelpers.findClass("android.content.pm.PackageParser.Package", null);
+        final Class AppOpsService = XposedHelpers.findClass("com.android.server.AppOpsService", null);
+
+
+        XposedBridge.hookAllMethods(AppOpsService, "isSystemOrPrivApp", new XC_MethodHook() {
+            protected void beforeHookedMethod(MethodHookParam paramAnonymousMethodHookParam)
+                    throws Throwable {
+                prefs.reload();
+                if (prefs.getBoolean("authcreak", true)) {
+                    paramAnonymousMethodHookParam.setResult(true);
+                }
+            }
+        });
+
+        XposedBridge.hookAllMethods(packageParser, "getApkSigningVersion", XC_MethodReplacement.returnConstant(1));
+
+        XposedBridge.hookAllConstructors(strictJarVerifier, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                prefs.reload();
+                if (prefs.getBoolean("authcreak", true)) {
+                    param.args[3] = false;
+                }
+            }
+        });
+
+        XposedBridge.hookAllConstructors(ApkSignatureSchemeV2Verifier, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Object packageInfoLite = param.thisObject;
+                prefs.reload();
+                if (prefs.getBoolean("authcreak", true)) {
+                    Field field = packageClass.getField(" SF_ATTRIBUTE_ANDROID_APK_SIGNED_ID");
+                    field.setAccessible(true);
+                    field.set(packageInfoLite, 3);
                 }
             }
         });
 
     }
 
-    @Override
-    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) {
-    }
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam paramLoadPackageParam) {
 
         if (("android".equals(paramLoadPackageParam.packageName)) && (paramLoadPackageParam.processName.equals("android"))) {
 
-            Class localClass = XposedHelpers.findClass("com.android.server.pm.PackageManagerService", paramLoadPackageParam.classLoader);
+            final Class localClass = XposedHelpers.findClass("com.android.server.pm.PackageManagerService", paramLoadPackageParam.classLoader);
             final Class packageClass = XposedHelpers.findClass("android.content.pm.PackageParser.Package", paramLoadPackageParam.classLoader);
 
             XposedBridge.hookAllMethods(localClass, "checkDowngrade", new XC_MethodHook() {
@@ -58,11 +98,11 @@ public class CorePatch extends XposedHelper implements IModule {
                     super.beforeHookedMethod(methodHookParam);
                     Object packageInfoLite = methodHookParam.args[0];
                     prefs.reload();
-                    if (prefs.getBoolean("downgrade", false)) {
+                    if (prefs.getBoolean("downgrade", true)) {
                         Field field = packageClass.getField("mVersionCode");
+                        field.setAccessible(true);
                         field.set(packageInfoLite, 0);
                     }
-                    ;
                 }
             });
 
@@ -70,7 +110,7 @@ public class CorePatch extends XposedHelper implements IModule {
 
                 protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
                     prefs.reload();
-                    if (prefs.getBoolean("authcreak", false)) {
+                    if (prefs.getBoolean("authcreak", true)) {
                         methodHookParam.setResult(true);
                     }
                 }
@@ -79,29 +119,43 @@ public class CorePatch extends XposedHelper implements IModule {
             XposedBridge.hookAllMethods(localClass, "compareSignatures", new XC_MethodHook() {
                 protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
                     prefs.reload();
-                    if (prefs.getBoolean("zipauthcreak", false)) {
+                    if (prefs.getBoolean("zipauthcreak", true)) {
                         methodHookParam.setResult(0);
                     }
                 }
             });
 
             XposedBridge.hookAllMethods(localClass, "compareSignaturesCompat", new XC_MethodHook() {
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam paramAnonymousMethodHookParam) {
                     prefs.reload();
-                    if (prefs.getBoolean("authcreak", false)) {
-                        methodHookParam.setResult(0);
+                    if (prefs.getBoolean("authcreak", true)) {
+                        paramAnonymousMethodHookParam.setResult(0);
+                    }
+                }
+            });
+            XposedBridge.hookAllMethods(localClass, "compareSignaturesRecover", new XC_MethodHook() {
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam paramAnonymousMethodHookParam) {
+                    prefs.reload();
+                    if (prefs.getBoolean("authcreak", true)) {
+                        paramAnonymousMethodHookParam.setResult(0);
                     }
                 }
             });
 
-            XposedBridge.hookAllMethods(localClass, "compareSignaturesRecover", new XC_MethodHook() {
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+            final Class AppOpsService = XposedHelpers.findClass("com.android.server.AppOpsService", null);
+
+
+            XposedBridge.hookAllMethods(AppOpsService, "isSystemOrPrivApp", new XC_MethodHook() {
+                protected void beforeHookedMethod(MethodHookParam paramAnonymousMethodHookParam)
+                        throws Throwable {
                     prefs.reload();
-                    if (prefs.getBoolean("authcreak", false)) {
-                        methodHookParam.setResult(0);
+                    if (prefs.getBoolean("authcreak", true)) {
+                        paramAnonymousMethodHookParam.setResult(true);
                     }
                 }
             });
+
         }
     }
+
 }
