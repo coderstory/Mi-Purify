9a3698808cb6e718f300ecaf09d34ea6b2587e5c
coderstory <coderstory@163.com>
2018-01-29 16:22:55 +0800

    禁止xposed hook 自身

--- app/src/main/java/com/coderstory/Purify/utils/Application.java
+++ app/src/main/java/com/coderstory/Purify/utils/Application.java
@@ -1,6 +1,8 @@
 package com.coderstory.Purify.utils;
 
 
+import java.lang.reflect.Field;
+
 public class Application extends android.app.Application {
 
     @Override
@@ -8,6 +10,15 @@ public class Application extends android.app.Application {
         super.onCreate();
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(getApplicationContext());
+        try {
+            Field v0_1 = ClassLoader.getSystemClassLoader()
+                    .loadClass("de.robv.android.xposed.XposedBridge")
+                    .getDeclaredField("disableHooks");
+            v0_1.setAccessible(true);
+            v0_1.set(null, true);
+        }
+        catch(Throwable v0) {
+        }
     }
 
 }
