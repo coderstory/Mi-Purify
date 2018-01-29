b6f0fdbd05728cbbaa34b636b3ce1c146d8cc031
coderstory <coderstory@163.com>
2018-01-29 16:23:30 +0800

    修复getExternalStorageDirectory引发的空指针

--- app/src/main/java/com/coderstory/Purify/config/Misc.java
+++ app/src/main/java/com/coderstory/Purify/config/Misc.java
@@ -1,11 +1,8 @@
 package com.coderstory.Purify.config;
 
-import android.os.Environment;
-
 public class Misc {
-    public static final String BackPath = Environment.getExternalStorageDirectory().getPath() + "/MIUI_Purify/Backup/";
+    public static final String BackPath = "/sdcard/MIUI_Purify/Backup/";
     public static final String MyBlogUrl = "http://blog.coderstory.cn";
-    public static final String BackUpFileName = Environment.getExternalStorageDirectory().getPath() + "/MIUI_Purify/Backup/";
     public static final String ApplicationName = "com.coderstory.Purify";
     public static final String SharedPreferencesName = "UserSettings";
     public static final String HostFileTmpName = "/hosts";
