e68b1790d4a636642ce3c3da263497a858265788
coderstory <coderstory@163.com>
2018-02-06 19:52:40 +0800

    1

--- app/src/main/java/com/coderstory/Purify/config/Misc.java
+++ app/src/main/java/com/coderstory/Purify/config/Misc.java
@@ -2,7 +2,7 @@ package com.coderstory.Purify.config;
 
 
 public class Misc {
-    public static final String BasePath = "/sdcard//MIUI_Purify";
+    public static final String BasePath = "/sdcard/MIUI_Purify";
     public static final String BackPath = BasePath + "/Backup/";
     public static final String CrashFilePath = BasePath + "/CrashLog/";
     public static final String MyBlogUrl = "http://blog.coderstory.cn";
