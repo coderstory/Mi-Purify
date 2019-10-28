ca4a5cce4400681373fc909f17331f7717a76dd7
coderstory <coderstory@163.com>
2019-10-28 21:41:05 +0800

    修复隐藏桌面图标

--- app/src/main/java/com/coderstory/Purify/module/HideApp.java
+++ app/src/main/java/com/coderstory/Purify/module/HideApp.java
@@ -1,8 +1,6 @@
 package com.coderstory.purify.module;
 
 import android.content.ComponentName;
-import android.content.Context;
-import android.util.Log;
 
 import com.coderstory.purify.plugins.IModule;
 import com.coderstory.purify.utils.XposedHelper;
@@ -15,25 +13,29 @@ import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-import static android.util.Config.LOGD;
 
 public class HideApp extends XposedHelper implements IModule {
 
 
+    /**
+     * mSkippedItems.add(new ComponentName("com.google.android.gms", "com.google.android.gms.app.settings.GoogleSettingsActivity"));
+     *
+     * @param loadPackageParam
+     */
     @Override
+    @SuppressWarnings("unchecked")
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
         final String value = prefs.getString("Hide_App_List", "");
         if (!value.equals("")) {
             final List<String> hideAppList = Arrays.asList(value.split(":"));
             if (loadPackageParam.packageName.equals("com.miui.home")) {
                 XposedBridge.log("load config" + value);
-                findAndHookMethod("com.miui.home.launcher.LauncherProvider", loadPackageParam.classLoader, "loadSkippedItems", Context.class, new XC_MethodHook() {
+                findAndHookMethod("com.miui.home.launcher.AppFilter", loadPackageParam.classLoader, "newInstance", new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) {
-                        HashSet<ComponentName> mSkippedItems = (HashSet<ComponentName>) XposedHelpers.getObjectField(param.thisObject, "mSkippedItems");
+
+                        HashSet<ComponentName> mSkippedItems = (HashSet<ComponentName>) XposedHelpers.getStaticObjectField(findClass("com.miui.home.launcher.AppFilter", loadPackageParam.classLoader), "mSkippedItems");
                         for (int i = 1; i < hideAppList.size(); i++) {
                             mSkippedItems.add(new ComponentName(hideAppList.get(i).split("&")[0], hideAppList.get(i).split("&")[1]));
                         }
